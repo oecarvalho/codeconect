5d1312f8ff28201ccde82ac9fb9392bee8994dfa
felipecarvalho <felipe.carvalho_2016@outlook.com>
2024-03-22 21:14:21 -0300

    first commit

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,9 @@
+import Logo from '../Sidebar/assets/codeconect-logo.svg';
+
+export default function Sidebar() {
+  return (
+    <aside>
+      <img src={Logo} alt="Logo do CodeConect" />
+    </aside>
+  );
+}
