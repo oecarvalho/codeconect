85930c18913e6b5613e993311421c3769ee6b186
felipecarvalho <felipe.carvalho_2016@outlook.com>
2024-04-08 00:46:12 -0300

    finalizando estrutura do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import BarraDePesquisa from './components/BarraPesquisa/index.jsx'
 import Sidebar from './components/Sidebar/index.jsx'
-
+import './app.css';
 function App() {
-  return(<Sidebar/>)
+  return(
+    <div className='container'>
+      <Sidebar/>
+      <BarraDePesquisa/>
+    </div>
+  
+  )
 }
 
 export default App
