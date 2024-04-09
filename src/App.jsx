22b6ef874df684bcd5487fc523a4e3b9bc7df20b
felipecarvalho <felipe.carvalho_2016@outlook.com>
2024-04-08 23:03:23 -0300

    adicionando filtragem

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import BarraDePesquisa from './components/BarraPesquisa/index.jsx'
 import Sidebar from './components/Sidebar/index.jsx'
 import './app.css';
+import Filtro from './components/Filtro/index.jsx';
+
 function App() {
   return(
     <div className='container'>
       <Sidebar/>
-      <BarraDePesquisa/>
+      
+      <div>
+        <BarraDePesquisa/>
+        <Filtro/>
+      </div>
+      
     </div>
   
   )
